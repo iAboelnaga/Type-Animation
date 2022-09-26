8673ad07644874902184e7c377cc862d13cfb4c6
Ahmed Aboelnaga <iaboelnaga97@gmail.com>
2022-09-26 13:10:41 +0200

    add index variables

--- TypeAnimationDemo/ViewController.swift
+++ TypeAnimationDemo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //create lable to display locations
+    //lable to display locations
     let locationLabel: UILabel = {
         let label = UILabel()
         label.text = "fhgfhfdhjhgjhgf"
@@ -21,13 +21,28 @@ class ViewController: UIViewController {
         return label
     }()
     
+    //variables for locations name
+    var locationSemiText = ""
+    var locationText = "Sahl Hasheesh"
+    let locationsTextArray = ["Sahl Hasheesh", "Cairo", "El Gouna", "North Coast"]
+    
+    var characterIndex: Int = 0 //variable to store the character index to start the reverse animation
+    var stringIndex: Int = 0 { //variable to store the location index to start the it`s animation
+        didSet {
+            changeLocationName() //change to next location name whenever stringIndex changed
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(locationLabel)
         locationLabel.frame = view.frame
     }
-
-
+    
+    //change to next location name
+    func changeLocationName(){
+        self.locationText = locationsTextArray[stringIndex]
+    }
 }
 
